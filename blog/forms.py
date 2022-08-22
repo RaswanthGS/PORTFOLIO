56d66d10865588c22457f07fc85e74da01e5c085
RaswanthGS <raswanth47@gmail.com>
2022-08-22 20:29:59 +0530

    Initial commit

---
+++ blog/forms.py
@@ -0,0 +1,14 @@
+from tkinter.ttk import Style
+from django import forms
+from django.forms import ModelForm, widgets
+from . import models
+
+class PostForm(ModelForm):
+    class Meta:
+        model = models.contact
+        fields = '__all__'
+        widgets = {
+            'name' : forms.TextInput(attrs = {'class':'field', 'style':'margin-bottom: 20px;'}),
+            'email' : forms.EmailInput(attrs = {'class':'field', 'style':'margin-bottom: 20px;'}),
+            'message' : forms.Textarea(attrs = {'class':'field', 'style':'margin-bottom: 20px;', 'rows':3})
+        }
